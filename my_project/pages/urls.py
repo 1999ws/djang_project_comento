8b94d789951f8a086cd32946dfc8b8677c125467
1999ws <watersoul1@naver.com>
2024-01-02 23:02:48 +0900

    코멘토 장고 프로젝트
    2024.01.02 23:02.

--- my_project/pages/urls.py
+++ my_project/pages/urls.py
@@ -4,4 +4,14 @@ urlpatterns = [
     path('',views.main),
     path('main/',views.main),
     path('opinion/',views.opinion),
-]
+    path('opinion_1/',views.opinion_1),
+    path('opinion_1_article1/',views.opinion_1_article1),
+    path('opinion_2/',views.opinion_2),
+    path('politics/',views.policits),
+    path('public/',views.public),
+    path('economy/',views.economy),
+    path('sports/',views.sports),
+    path('entertainment/',views.entertainment),
+    path('login/',views.login),
+    path('signup/',views.signup),
+]   
--- my_project/pages/views.py
+++ my_project/pages/views.py
@@ -6,3 +6,25 @@ def main(request):
     return render(request,'pages\main.html')
 def opinion(request):
     return render(request,'pages\opinion.html')
+def opinion_1(request):
+    return render(request,'pages\opinion_1.html')
+def opinion_1_article1(request):
+    return render(request,'pages\opinion_1_article1.html')
+def opinion_2(request):
+    return render(request,'pages\opinion_2.html')
+def policits(request):
+    return render(request,'pages\politics.html')
+def opinion(request):
+    return render(request,'pages\opinion.html')
+def public(request):
+    return render(request,'pages\public.html')
+def economy(request):
+    return render(request,'pages\economy.html')
+def sports(request):
+    return render(request,'pages\sports.html')
+def entertainment(request):
+    return render(request,'pages\entertainment.html')
+def login(request):
+    return render(request,'pages\login.html')
+def signup(request):
+    return render(request,'pages\signup.html')
